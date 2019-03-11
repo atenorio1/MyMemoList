3bfc893b297c834629106bcd4d8bc27c2e01750a
Alejandro Tenorio <Alejandro.Tenorio14@gmail.com>
2019-03-11 01:07:33 -0400

    added 2 more activities (memo activity, and memo settings activity) along with their layout pages

--- app/src/main/java/com/example/mymemolist/MemoSettingsActivity.java
+++ app/src/main/java/com/example/mymemolist/MemoSettingsActivity.java
@@ -1,7 +1,9 @@
 package com.example.mymemolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MemoSettingsActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MemoSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_settings);
+
+        initListButton();
+        initSettingsButton();
+    }
+
+    // Method that initializes the list image button
+    private void initListButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(v -> {
+            Intent intent = new Intent(MemoSettingsActivity.this, MemoListActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
+    }
+
+    // Method that initializes the settings image button
+    private void initSettingsButton() {
+        ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
+        ibSettings.setEnabled(false);
     }
 }
