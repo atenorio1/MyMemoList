fc1cd4eba511bb9d34934909b9dd328aad46e189
Alejandro Tenorio <Alejandro.Tenorio14@gmail.com>
2019-03-10 20:30:00 -0400

    added 2 more activities (memo activity, and memo settings activity) along with their layout pages

--- app/src/main/java/com/example/mymemolist/MemoSettingsActivity.java
+++ app/src/main/java/com/example/mymemolist/MemoSettingsActivity.java
@@ -3,12 +3,11 @@ package com.example.mymemolist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MyMemoList extends AppCompatActivity {
+public class MemoSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_memo_list);
+        setContentView(R.layout.activity_memo_settings);
     }
-
 }
